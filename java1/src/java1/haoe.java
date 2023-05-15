12b4e08c2f7146be15a3355c24936583d1e43a03
sushant <sushank3008@gmail.com>
2023-05-15 18:40:29 +0530

    2nd commit

--- java1/src/java1/haoe.java
+++ java1/src/java1/haoe.java
@@ -3,9 +3,9 @@ package java1;
 public class haoe {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("priyanka");
 		
-		System.out.println("Hello java");
+		System.out.println("tornekar");
 
 	}
 
